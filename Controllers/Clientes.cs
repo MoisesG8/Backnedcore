e8d957a564ac177bcf5183ce74dd2d506f84896c
MoiG_8 <moigonzales1061@gmail.com>
2019-11-08 13:14:43 -0600

    changes

--- Controllers/Clientes.cs
+++ Controllers/Clientes.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BackendTienda.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class Clientes : Controller
     {
         //metodo para login

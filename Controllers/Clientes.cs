886bc348551ae51132d98e463c592af6609db919
MoiG_8 <moigonzales1061@gmail.com>
2019-11-08 12:49:00 -0600

    first commit

---
+++ Controllers/Clientes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendTienda.Modelos;
+using BackendTienda.Negocio;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackendTienda.Controllers
+{
+    [Route("api/[controller]")]
+    public class Clientes : Controller
+    {
+        //metodo para login
+        [HttpGet("ClientLogin")]
+        public JsonResult LoginClient(string email, string pass)
+        {
+            ClientesModel InfClientesLgn = new ClientesModel();
+            InfClientesLgn = Clientes_Business.LoginClientes(email, pass);
+            return Json(InfClientesLgn);
+        }
+        //metodo para registrar un nuevo cliente
+        [HttpPost("Registrar")]
+        public JsonResult RegistrarCliente(ClientesModel regisCliente)
+        {
+            Clientes_Business.Registrar(regisCliente);
+            return Json(regisCliente);
+        }
+    }
+}
